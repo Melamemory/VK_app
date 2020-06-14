ab9affb4c8624592136c01bdf50323bfc996cfab
Iuliia Ishmurzina <ishmurzina.julia@gmail.com>
2020-06-15 02:06:10 +0300

    L1_HW_Ishmurzina

---
+++ VK_app/VK_app/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  VK_app
+//
+//  Created by Юлия Ишмурзина on 14.06.2020.
+//  Copyright © 2020 Юлия Ишмурзина. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
